47f6c06d1f3c91bdb84f7947324f8e3b18931ba5
Sueyun Bae <sueyun8747@gmai.com>
2025-12-16 11:06:30 +0900

    251216

--- src/main/java/com/sist/web/controller/MainController.java
+++ src/main/java/com/sist/web/controller/MainController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("/")
-	public String main_page(Model model)
-	{
-		model.addAttribute("main_html","main/home");
-		return "main/main";
-	}
+   @GetMapping("/")
+   public String main_page(Model model)
+   {
+	   model.addAttribute("main_html", "main/home");
+	   return "main/main";
+   }
 }
--- src/main/java/com/sist/web/entity/BoardEntity.java
+++ src/main/java/com/sist/web/entity/BoardEntity.java
@@ -1,17 +1,37 @@
 package com.sist.web.entity;
+/*
+ *   NO      NOT NULL NUMBER         
+NAME    NOT NULL VARCHAR2(51)   
+SUBJECT NOT NULL VARCHAR2(4000) 
+CONTENT NOT NULL CLOB           
+PWD     NOT NULL VARCHAR2(10)   
+REGDATE          DATE           
+HIT              NUMBER         
+
+ */
 import java.util.*;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
-// save(), delete()
-// findByNo(int no) WHERE no=1
+// save()  ,  delete()
+// findByNo(int no) WHERE no=1 
+// findByName(String name) 
+// findByNameLike
+// 단점 : JOIN / SubQuery를 지원하지 않는다
 @Data
 @Entity(name="board")
 public class BoardEntity {
-	@Id
+    @Id
 	private int no;
-	private int hit;	
-	private String name,subject,content,pwd;
-	private Date regdate;
+    
+    private int hit;
+    private String name,subject,content,pwd;
+    private Date regdate;
 }
+
+
+
+
+
+
